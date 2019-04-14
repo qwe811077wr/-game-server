69a63b0e5a497376b05f40eca277e918c1e747c6
cxx <469350450@qq.com>
2019-04-15 00:25:38 +0800

    获取在线玩家信息工具

---
+++ tools/onlineAvatar.js
@@ -0,0 +1,50 @@
+/**
+ * Date: 2019/4/14
+ * Author: admin
+ * Description: 在线玩家 [cmd: node onlineAvatar]
+ */
+let connectToMaster = require('./gameOperationConnect').connectToMaster;
+
+function onlineAvatar(opts) {
+    let id = 'pomelo_online_avatar_' + Date.now();
+    connectToMaster(id, opts, function (client) {
+        client.request('onlineUser', null, function (err, msg) {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                let totalConnCount = 0, loginedCount = 0, list = [];
+                var msg2 = msg.body;
+                for(var sid in msg2) {
+                    totalConnCount += msg2[sid].totalConnCount;
+                    loginedCount += msg2[sid].loginedCount;
+                    var lists = msg2[sid].loginedList;
+                    for(var i=0;i<lists.length;i++){
+                        list.push({
+                            address : lists[i].address,
+                            serverId : sid,
+                            username : lists[i].username,
+                            loginTime : new Date(parseInt(lists[i].loginTime)).toLocaleString().replace(/年|月/g, "-").replace(/日/g, " "),
+                            uid : lists[i].uid
+                        });
+                    }
+                }	
+                
+                console.log(list);
+                console.log('totalConnCount = ', totalConnCount);
+                console.log('onlineCount = ', loginedCount);
+            }
+            process.exit(0);
+        })
+    });
+};
+
+let arguments = process.argv.splice(2);
+let opts = {
+    username: arguments[1],
+    password: arguments[2],
+    host: arguments[3],
+    port: arguments[4],
+};
+
+onlineAvatar(opts);
