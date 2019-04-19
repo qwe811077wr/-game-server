61603eca944414ea893f3082562116407060a6eb
cxx <469350450@qq.com>
2019-04-19 08:57:26 +0800

    update

--- app/common/stage.js
+++ app/common/stage.js
@@ -2,14 +2,14 @@
 // 对应consts表中游戏类型与阶梯数量
 module.exports = {
 	[1]: [
-		{underScore: 100, bArea: 200, eArea: -1, peopleNum: 5000},
-		{underScore: 1000, bArea: 2000, eArea: -1, peopleNum: 5000},
-		{underScore: 10000, bArea: 20000, eArea: -1, peopleNum: 5000},
+		{underScore: 100, bArea: 1000, eArea: -1, peopleNum: 100},
+		{underScore: 1000, bArea: 10000, eArea: -1, peopleNum: 100},
+		{underScore: 10000, bArea: 100000, eArea: -1, peopleNum: 100},
 	],
 	
 	[2]: [
-		{underScore: 100, bArea: 200, eArea: -1, peopleNum: 5000},
-		{underScore: 1000, bArea: 2000, eArea: -1, peopleNum: 5000},
-		{underScore: 10000, bArea: 20000, eArea: -1, peopleNum: 5000},
+		{underScore: 100, bArea: 1000, eArea: -1, peopleNum: 100},
+		{underScore: 1000, bArea: 10000, eArea: -1, peopleNum: 100},
+		{underScore: 10000, bArea: 100000, eArea: -1, peopleNum: 100},
 	]
 }
